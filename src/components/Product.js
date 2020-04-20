4fc910f84f023d7e76bf141c6a64647053138df9
bobrock8 <aleksandarilic990@gmail.com>
2020-04-20 16:13:21 +0200

    product page

--- src/components/Product.js
+++ src/components/Product.js
@@ -8,15 +8,15 @@ class Product extends Component {
     render() {
         const {id, title, img, price, inCart} = this.props.product;
         return (
-            <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-4 my-3">
+            <ProductWrapper className="col-12 mx-auto col-md-6 col-lg-4 my-3">
                 <div className="card">
                     <ProductConsumer>
 
                     {value => (
 
-                        <div className="img-container p-5" onClick={() =>value.handleDetail(id)}>
+                        <div className="img-container text-center p-5" onClick={() =>value.handleDetail(id)}>
                             <Link to="/details">
-                                <img src={img} alt="product" className="card-img-top"/>
+                                <img src={img} alt="product" className="card-img-top img-fluid"/>
                             </Link>
                             <button className="card-btn" disabled={inCart ? true : false} onClick={
                                 () => {
